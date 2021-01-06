10e2c737d3902e95cb0ed003e37ddf87b7e96c8a
Marko Fritzsche <marko.fritzsche@emarsys.com>
2021-01-06 22:03:38 +0100

    INTERNAL: initial commit

--- anachron.sh
+++
@@ -1,4 +0,0 @@
-
-if [ -f /app/anacrontab ]; then
-  /app/.heroku/vendor/anacron/anacron -t /app/anacrontab
-fi
---
+++ anacron.sh
@@ -0,0 +1,4 @@
+
+if [ -f /app/.heroku/vendor/anacron/cron ]; then
+  /app/.heroku/vendor/anacron/cron
+fi
