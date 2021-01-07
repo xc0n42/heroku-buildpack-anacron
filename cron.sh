dc5ddd8c71c4befb9f0fe89bb002099230866aac
Marko Fritzsche <marko.fritzsche@emarsys.com>
2021-01-07 14:51:20 +0100

    INTERNAL: initial commit

--- cron.sh
+++ cron.sh
@@ -1,4 +1,16 @@
 
+if [ ! -d /app/cron ]; then
+  mkdir /app/cron
+fi
+
+if [ ! -d /app/cron/crontabs ]; then
+  mkdir /app/cron/crontabs
+fi
+
+if [ ! -f /app/crontab ]; then
+  touch /app/crontab
+fi
+
 if [ -f /app/.heroku/vendor/anacron/cron ]; then
   /app/.heroku/vendor/anacron/cron
 fi
